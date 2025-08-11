8bbb3f57262d03a5f744fae63e2889422033ecd7
amanda rodrigues de mattos <amandalaryssarodrigues@hotmail.com>
2025-08-11 00:49:06 -0300

    Delete projeto-k8s-deploy directory

--- projeto-k8s-deploy/backend/models.py
+++
@@ -1,7 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-class Mensagem(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    texto = db.Column(db.String(200), nullable=False)
